e666ba429d9f3494fadb36439e77c3910bd03990
Kirill Kayumov <kirill.kayumov@flatstack.com>
2014-10-11 15:23:31 +0400

    Add User model

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module SeaBattle
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.host = 'localhost:5000'
   end
 end
