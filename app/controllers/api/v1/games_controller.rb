831cda53dc08a1c3fa94c896a183ca99852f09fa
Kirill Kayumov <kirill.kayumov@flatstack.com>
2014-10-14 09:28:59 +0400

    Add new method to GamesController and change create method

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,7 +1,24 @@
 class GamesController < ApplicationController
-  def create
+  def new
     Game.create(user1_id: params[:requester_id],
                 user2_id: params[:receiver_id])
-    render status: :created
+  end
+
+  def create
+    if params[:confirm] == 'true'
+      game = not_confirmed_game
+      game.update_attributes(status: Game::STATUS.invert['started'])
+    else
+      game = not_confirmed_game
+      game.destroy
+    end
+  end
+
+  private
+
+  def not_confirmed_game
+    Game.where(user1_id: params[:requester_id],
+               user2_id: params[:receiver_id],
+               status: Game::STATUS.invert['not confirmed']).first
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+  STATUS = {
+    0 => 'not confirmed',
+    1 => 'started',
+    2 => 'finished'
+  }
 end
