2f23189726b83dc80ed6f4eaf05210e8c3775e56
Aydar Farrakhov <stratnniknm@gmail.com>
2014-10-14 11:59:45 +0400

    Fix 2

--- app/assets/javascripts/MainCtrl.js
+++ app/assets/javascripts/MainCtrl.js
@@ -146,6 +146,8 @@ app.controller('MainCtrl', ['$scope', 'II', 'HelpService', 'FieldState', '$http'
         $scope.II.initialize();
         $scope.yourField = $scope.II.returnField().slice(0);
         $scope.yourShips= ["", 0, 0, 0, 0];
+        $scope.II.initialize();
+        $scope.IIfield = $scope.II.returnField();
     }
 
 }]);
