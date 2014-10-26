66bae6ee839e2b74b272ed53efb374cdd85ff2fa
Kirill Kayumov <kirill.kayumov@flatstack.com>
2014-10-26 12:44:41 +0300

    Add nickname

--- db/migrate/20141011110124_devise_create_users.rb
+++ db/migrate/20141011110124_devise_create_users.rb
@@ -1,19 +1,14 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :nickname
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Rememberable
       t.datetime :remember_created_at
-
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
 
       t.datetime :last_active_at
 
